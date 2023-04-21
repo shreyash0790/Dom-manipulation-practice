7f0b48f105420cd6945362dbbd20ad7c4346c59f
Shreyaskar <shreyash0790@gmail.com>
2023-04-22 04:15:43 +0530

    Querryselector/ALL

--- dom.js
+++ dom.js
@@ -22,9 +22,35 @@ headertitle.textContent='New parts';
 // }
 // newhead[2].style.backgroundColor='green';
 
-var li=document.getElementsByTagName('li');
-console.log(li);
-for(var i=0;i<li.length;i++){
-    li[i].style.fontWeight='bold';
+// var li=document.getElementsByTagName('li');
+// console.log(li);
+// for(var i=0;i<li.length;i++){
+//     li[i].style.fontWeight='bold';
+// }
+// li[3].style.backgroundColor='green';
+
+// var input=document.querySelector('input');
+// input.value='Type our text here';
+
+
+// var submit=document.querySelector('input[type="submit"]');
+// submit.value='SEND';
+
+// var items=document.querySelector('.list-group-item:nth-child(2)');
+// items.style.backgroundColor='green';
+
+// var items=document.querySelector('.list-group-item:nth-child(3)');
+// items.style.color='white';
+
+var odd=document.querySelectorAll('li:nth-child(odd)');
+
+for(var i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor='green';
 }
-li[3].style.backgroundColor='green';
+
+var items=document.querySelectorAll('.list-group-item');
+console.log(items);
+items[1].style.color='green';
+
+
+
