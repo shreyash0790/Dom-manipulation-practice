25dd20bfc3650f4b65a33b8ce8655e4e0b21bab2
Shreyaskar <shreyash0790@gmail.com>
2023-04-23 10:50:11 +0530

    dom part 2

--- dom.js
+++ dom.js
@@ -1,11 +1,11 @@
-console.dir(document);
-document.title="Pc parts Revised";
-console.log(document.doctype);
-console.log(document.forms);
-console.log(document.links);
+// console.dir(document);
+// document.title="Pc parts Revised";
+// console.log(document.doctype);
+// console.log(document.forms);
+// console.log(document.links);
 
-var headertitle=(document.getElementById('header-title'));
-headertitle.textContent='New parts';
+// var headertitle=(document.getElementById('header-title'));
+// headertitle.textContent='New parts';
 // headertitle.innertext='Goodbye';
 
 // headertitle.innerHTML='<h3>New Parts</h3>';
@@ -42,15 +42,85 @@ headertitle.textContent='New parts';
 // var items=document.querySelector('.list-group-item:nth-child(3)');
 // items.style.color='white';
 
-var odd=document.querySelectorAll('li:nth-child(odd)');
+// var odd=document.querySelectorAll('li:nth-child(odd)');
 
-for(var i=0;i<odd.length;i++){
-    odd[i].style.backgroundColor='green';
-}
+// for(var i=0;i<odd.length;i++){
+//     odd[i].style.backgroundColor='green';
+// }
 
-var items=document.querySelectorAll('.list-group-item');
-console.log(items);
-items[1].style.color='green';
+// var items=document.querySelectorAll('.list-group-item');
+// console.log(items);
+// items[1].style.color='green';
 
+// Transversing the dom
+
+// var itemlist=document.querySelector('#items');
+// // parent node
+// console.log(itemlist.parentNode);
+// itemlist.parentNode.style.backgroundColor='#f4f4f4';
+
+// parent element 
+
+// var itemlist=document.querySelector('#items');
+// // parent node
+// console.log(itemlist.parentElement);
+// itemlist.parentElement.style.backgroundColor='#f4f4f4';
+
+// // children node 
+
+// console.log(itemlist.children);
+// itemlist.children[2].style.backgroundColor='yellow';
+
+// // first child
+
+// console.log(itemlist.firstChild);
+
+// first element child 
+
+// console.log(itemlist.firstElementChild);
+// itemlist.firstElementChild.textContent='hellos';
+
+// last element child
+
+// itemlist.lastElementChild.textContent='hellos boys';
+
+// next element siblings 
+
+// itemlist.nextElementSibling.textContent='hello';
+
+// previous element siblings 
+
+// console.log(itemlist.previousElementSibling);
+
+// create element 
+
+
+// var newdiv=document.createElement('div');
+
+// // add class
+// newdiv.className='hello';
+
+// // add id
+// newdiv.id='hello1';
+
+// // add attr
+// newdiv.setAttribute=('title','hello div');
+
+// // create node
+// var newdivtext=document.createTextNode('HEllo');
+
+// // add text to div
+
+// newdiv.appendChild(newdivtext);
+
+// // adding craeted to webpage
+
+// var contain=document.querySelector('header .container'); 
+// // this means in the header we want conatiner class
+
+// var h=document.querySelector('header h1');
+
+// contain.insertBefore(newdiv,h);
+// //this takes what we are inserting and we are inserting before
 
 
